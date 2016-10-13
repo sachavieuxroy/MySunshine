ac607804cf40453638e1d7ad3dd4e726d2a55b21
sachavieuxroy <mail@sachavieuxroy.com>
2016-10-13 11:53:42 +0200

    implemented OpenWeatherMap Webservice

--- app/src/main/java/android/example/com/mysunshine/ForecastFragment.java
+++ app/src/main/java/android/example/com/mysunshine/ForecastFragment.java
@@ -42,45 +42,43 @@ public class ForecastFragment extends Fragment implements IFetchWeatherTaskCallB
             ,"Teusday - Rainy - 36/11"
     };
     private final String LOG_TAG = ForecastFragment.class.getSimpleName();
+    private ArrayAdapter<String> mForecastAdapter;
+    private List<String> weekForecast;
+    private ListView listView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
+
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Create some dummy data for the ListView.  Here's a sample weekly forecast
-        String[] data = {
-                "Mon 6/23 - Sunny - 31/17",
-                "Tue 6/24 - Foggy - 21/8",
-                "Wed 6/25 - Cloudy - 22/17",
-                "Thurs 6/26 - Rainy - 18/11",
-                "Fri 6/27 - Foggy - 21/10",
-                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
-                "Sun 6/29 - Sunny - 20/7"
-        };
-        List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
-
-
-        // Now that we have some dummy forecast data, create an ArrayAdapter.
-        // The ArrayAdapter will take data from a source (like our dummy forecast) and
-        // use it to populate the ListView it's attached to.
-        ArrayAdapter<String> mForecastAdapter =
-                new ArrayAdapter<String>(
-                        getActivity(), // The current context (this activity)
-                        R.layout.list_item_forcast, // The name of the layout ID.
-                        R.id.list_item_forecast_textview, // The ID of the textview to populate.
-                        weekForecast);
-
+//         String[] data = {
+//                "Mon 6/23 - Sunny - 31/17",
+//                "Tue 6/24 - Foggy - 21/8",
+//                "Wed 6/25 - Cloudy - 22/17",
+//                "Thurs 6/26 - Rainy - 18/11",
+//                "Fri 6/27 - Foggy - 21/10",
+//                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+//                "Sun 6/29 - Sunny - 20/7"
+//        };
+//        weekForecast = new ArrayList<String>(Arrays.asList(data));
+//        mForecastAdapter = new ArrayAdapter<String>(
+//               getActivity(), // The current context (this activity)
+//               R.layout.list_item_forcast, // The name of the layout ID.
+//               R.id.list_item_forecast_textview, // The ID of the textview to populate.
+//                weekForecast);
 
         View rootView = inflater.inflate(R.layout.fragment_main, container);
 
-        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
-        listView.setAdapter(mForecastAdapter);
+        listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+//        listView.setAdapter(mForecastAdapter);
 
+
+        refeshData();
 
         return rootView;
     }
@@ -113,7 +111,14 @@ public class ForecastFragment extends Fragment implements IFetchWeatherTaskCallB
 
     @Override
     public void FetchWeatherTaskCallBack(String[] data) {
-
+        weekForecast = new ArrayList<String>(Arrays.asList(data));
+        mForecastAdapter = new ArrayAdapter<String>(
+                getActivity(), // The current context (this activity)
+                R.layout.list_item_forcast, // The name of the layout ID.
+                R.id.list_item_forecast_textview, // The ID of the textview to populate.
+                weekForecast);
+        listView.setAdapter(mForecastAdapter);
+        mForecastAdapter.notifyDataSetChanged();
     }
 
     private class FetchWeatherTask extends AsyncTask<String,String,String[]>{
